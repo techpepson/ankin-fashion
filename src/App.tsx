bc4a6c6450f522f3eccd8e596cbcabd67d802487
Peprah Dickson Daniel <ddpeprah@st.ug.edu.gh>
2024-08-18 22:24:06 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,29 @@ import {
   Accessories,
   Home,
 } from "./components/component-export";
+import Navbar from "./components/Navbar.tsx"
 import "@radix-ui/themes/styles.css";
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/sidebar" element={<Sidebar />} />
-      <Route path="/add-product" element={<AddProduct />} />
-      <Route path="/women-items" element={<WomenItems />} />
-      <Route path="/men-items" element={<MenItems />} />
-      <Route path="/kid-items" element={<KidItems />} />
-      <Route path="/list-users" element={<ListUsers />} />
-      <Route path="/unisex-items" element={<UnisexItems />} />
-      <Route path="/all-products" element={<ListAllProducts />} />
-      <Route path="/accessories" element={<Accessories />} />
-      <Route path="/list-products/:id" element={<ListItems />} />
-    </Routes>
+    <div className="flex flex-wrap gap-1 p-1>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/women-items" element={<WomenItems />} />
+        <Route path="/men-items" element={<MenItems />} />
+        <Route path="/kid-items" element={<KidItems />} />
+        <Route path="/list-users" element={<ListUsers />} />
+        <Route path="/unisex-items" element={<UnisexItems />} />
+        <Route path="/all-products" element={<ListAllProducts />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/list-products/:id" element={<ListItems />} />
+      </Routes>
+    </div>
+    
   );
 };
 
