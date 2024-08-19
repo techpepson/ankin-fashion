71e654b5cfea20b4e7f44cda2ff585c76fad498a
techpepson <ddpeprah@st.ug.edu.gh>
2024-08-19 01:49:46 +0000

    feat(position header): add the header and the footer in the app.tsx file to be seen on every page.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,18 @@
 import React from "react";
-import { MenItems, WomenItems, KidItems, Accessories } from "./component-export";
+import {
+  MenItems,
+  WomenItems,
+  KidItems,
+  Accessories,
+} from "./component-export";
 
 const Home: React.FC = () => (
   <>
-    <div className="grid grid-cols-3">
-      <div>
-        <MenItems />
-      </div>
-      <div>
-        <WomenItems />
-      </div>
-      <div>
-        <KidItems />
-      </div>
-      <div>
-        <Accessories/>
-      </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+      <MenItems />
+      <WomenItems />
+      <KidItems />
+      <Accessories />
     </div>
   </>
 );
